4a5d62b111d4c34440bf3a023eb2663b1f7a99b0
Jithin-caz <117753804+Jithin-caz@users.noreply.github.com>
2024-03-14 00:46:22 +0530

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 "use client"
 import Navbar from "./navbar/page";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import AOS from 'aos'
+import 'aos/dist/aos.css';
 import Hero from "./hero/pages";
+import { useEffect } from "react";
 export default function Home() {
- 
+ useEffect(()=>{
+  AOS.init({
+  });
+ },[])
   return (
    <section>
 <Navbar/>
